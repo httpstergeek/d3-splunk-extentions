cca581990b33a0154278572279248f8b984d573a
bernie macias <berniem@zillow.com>
2015-08-25 15:46:21 -0700

    fix for description and view source bug

--- d3_splunk_extentions/appserver/static/autodiscover.js
+++ d3_splunk_extentions/appserver/static/autodiscover.js
@@ -1,12 +1,10 @@
-// Barrowed from simple_xml_examples app published by splunk
-
 require.config({
     paths: {
         "app": "../app"
     }
 });
 
-require(['splunkjs/mvc/d3_splunk_extentions/ready!'], function(){
+require(['splunkjs/mvc/simplexml/ready!'], function(){
     require(['splunkjs/ready!'], function(){
         // The splunkjs/ready loader script will automatically instantiate all elements
         // declared in the dashboard's HTML.
