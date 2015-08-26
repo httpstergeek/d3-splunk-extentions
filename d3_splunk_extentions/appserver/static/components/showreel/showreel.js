b0675ad04708313eaf7aa7c377c44548d86af4ce
bernie macias <berniem@zillow.com>
2015-08-26 11:24:01 -0700

    formatted data for viz

--- d3_splunk_extentions/appserver/static/components/showreel/showreel.js
+++ d3_splunk_extentions/appserver/static/components/showreel/showreel.js
@@ -24,10 +24,18 @@ define(function(require, exports, module) {
     },
     // Making the data look how we want it to for updateView to do its job
     formatData: function(data) {
-      var formattedData;
+      var formattedData = {data: []};
+      var parseDate  = d3.time.format.iso.parse;
+      console.log(data);
+      data.map(function(d) {
+        d._time = parseDate(d._time);
+        formattedData.data.push(d);
+      });
       return formattedData; // this is passed into updateView as 'data'
     },
     updateView: function(viz, data) {
+      console.log(data);
+
     }
   });
   return Showreel;
